86bd1f269eb689d5ec1f2c5e6b0363fb5a82db6e
chimok <mgq0314@gmail.com>
2018-04-14 10:46:29 +0800

    add readme

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './pages/home/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="">
+        豆瓣电影
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass">
+
+</style>
+
